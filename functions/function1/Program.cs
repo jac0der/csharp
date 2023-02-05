91e4dc8e6415af6b2830eeaaf5dd7063145f4f38
jacoder <mantle@vivaldi.net>
2023-02-05 06:58:00 -0500

    Added collections folder for arrays. Added a function

---
+++ functions/function1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+TryAgain();
+
+
+void TryAgain()
+{
+   Console.WriteLine("Please try again!!");
+}
+
+// function to return a value
+
+
+//function to accept parameter(s)
