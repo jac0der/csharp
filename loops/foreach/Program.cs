f59e2affba19201f9f8db17070c1b4c07afced96
jacoder <mantle@vivaldi.net>
2023-02-05 06:01:28 -0500

    LOOPS
    Add a for each loop to loop through the items of an array.

---
+++ loops/foreach/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// set all grades in 1 line by initializing
+var studentGrades = new int[]{ 80, 75, 95, 93, 88, 80};
+
+Console.WriteLine(studentGrades[4]);
+
+foreach(var grade in studentGrades)
+{
+    Console.WriteLine(grade);
+}
