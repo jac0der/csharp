78f50c188b0db4c1f2a83c6111cc5b5893a5253f
jacoder <mantle@vivaldi.net>
2023-02-05 05:31:50 -0500

    count to 10 and back down to1 5 times

--- loops/forloop/Program.cs
+++ loops/forloop/Program.cs
@@ -5,3 +5,22 @@ for(int i=0; i <= 10; i++)
 {
     Console.WriteLine(i);
 }
+
+// count from 1 to 10 then back down to 1 5 times
+int times = 1;
+
+while(times <= 5)
+{
+    for(int i=1; i <= 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+
+    for(int i=10; i > 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+
+    times++;
+}
+
