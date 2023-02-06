da39153a4dc8ab4530c0e054fbcfa667cba674db
jacoder <mantle@vivaldi.net>
2023-02-05 22:09:10 -0500

    VARIABLES
    defining coalescing variables.

---
+++ variables/Models/Person.cs
@@ -0,0 +1,5 @@
+class Person{
+    public string first_name { get; set; } = string.Empty;
+    public string last_name { get; set; } = string.Empty;
+
+};
--- variables/Program.cs
+++ variables/Program.cs
@@ -1,7 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// define Person object
+var person = new Person();
+
 // creating a nullable type
 int? year = null;
+
+
+/*
+    The C# double question mark operator ?? is known as the null 
+    coalescing operator.
+    In a nutshell, the null coalescing operator is "if the first 
+    value is null, use this other value."
+    Null coalescing operator is a binary operator that takes two 
+    operands: the first operand is an expression that can be null, 
+    and the second operand is the value that you want to assign to 
+    the expression if it is not null.
+*/
+var personValue = person ?? new Person();
+
 
 var my_string = "My String!";
 string current_weather = "rainy";
@@ -13,10 +30,7 @@ float valuation = (float)875.35;
 double value = 45.24;
 
 
-Console.WriteLine(age.ToString() + ' ' + cost.ToString());
 
-Console.ReadLine();
-Console.WriteLine(my_string);
 
 
 
